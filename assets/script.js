7024e10a03242d947502da754e87f906f121f373
BiancoGabry <supergabrielebianco@gmail.com>
2024-09-15 22:24:47 +0200

    menu quasi fixato 2

--- assets/script.js
+++ assets/script.js
@@ -139,11 +139,7 @@ anchorLinks.forEach((link) => {
 //menu hamburger
 function toggleMenu() {
     const menu = document.getElementById('menu');
-    if (menu.style.display === 'flex') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'flex';
-    }
+    menu.classList.toggle('active');
 }
 
 
